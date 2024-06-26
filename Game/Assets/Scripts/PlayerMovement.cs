6172ea384816c6efc2556e4f10b03a9312d618f6
AdminTroller <lauedmondyc@gmail.com>
2024-06-27 00:50:51 +0800

    map template work

--- Game/Assets/Scripts/PlayerMovement.cs
+++ Game/Assets/Scripts/PlayerMovement.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
-    float horizontal = 0;
 
-    const float speed = 5f;
+    const float speed = 7f;
 
     void Awake() {
         Application.targetFrameRate = 60;
@@ -16,10 +15,10 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate() {
         int hor = 0;
         int ver = 0;
-        if (Input.GetKey(KeyCode.UpArrow)) ver++;
-        if (Input.GetKey(KeyCode.DownArrow)) ver--;
-        if (Input.GetKey(KeyCode.LeftArrow)) hor--;
-        if (Input.GetKey(KeyCode.RightArrow)) hor++;
+        if (Input.GetKey(KeyCode.W)) ver++;
+        if (Input.GetKey(KeyCode.S)) ver--;
+        if (Input.GetKey(KeyCode.A)) hor--;
+        if (Input.GetKey(KeyCode.D)) hor++;
         
         rb.velocity = new Vector2(hor*speed, ver*speed);
     }
