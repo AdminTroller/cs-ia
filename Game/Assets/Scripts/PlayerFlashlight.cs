e9a7474bea6883e35d3455e8ae0a43a321cb2e58
AdminTroller <lauedmondyc@gmail.com>
2024-07-12 18:38:39 +0800

    shadows done

--- Game/Assets/Scripts/PlayerFlashlight.cs
+++ Game/Assets/Scripts/PlayerFlashlight.cs
@@ -4,6 +4,10 @@ using UnityEngine.Rendering.Universal;
 public class PlayerFlashlight : MonoBehaviour
 {
     [SerializeField] Light2D flashlight;
+    [SerializeField] AudioSource sound;
+    [SerializeField] AudioClip on;
+    [SerializeField] AudioClip off;
+    bool toggle = true;
 
     void Update() {
         Vector2 displacement = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
@@ -11,5 +15,13 @@ public class PlayerFlashlight : MonoBehaviour
         rotate = -Mathf.Atan(displacement.x/displacement.y) * Mathf.Rad2Deg;
         if (displacement.y < 0) rotate -= 180;
         flashlight.transform.eulerAngles = Vector3.forward * rotate;
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            toggle = !toggle;
+            if (toggle) sound.clip = on;
+            else sound.clip = off;
+            sound.Play();
+        }
+        flashlight.enabled = toggle;
     }
 }
