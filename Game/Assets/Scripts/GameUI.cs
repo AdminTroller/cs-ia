901ce199328e14430c9df86ceb1b59cf8f1b4dd6
AdminTroller <lauedmondyc@gmail.com>
2024-09-24 14:18:56 +0800

    idfk color

--- Game/Assets/Scripts/GameUI.cs
+++ Game/Assets/Scripts/GameUI.cs
@@ -8,9 +8,11 @@ public class GameUI : MonoBehaviour
     [SerializeField] Sprite[] batStates;
     [SerializeField] TextMeshProUGUI batNum;
     public float bat = 100;
+    public Color[] batColors;
 
     void Update() {
         batIcon.sprite = batStates[Mathf.CeilToInt(bat/25)];
         batNum.text = Mathf.RoundToInt(bat).ToString() + "%";
+        // batNum.color = batColors[Mathf.CeilToInt(bat/25)];
     }
 }
