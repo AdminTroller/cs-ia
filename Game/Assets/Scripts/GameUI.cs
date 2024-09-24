c00e33bb58edd4e7d635c5ed82250829dfa6f5a8
AdminTroller <lauedmondyc@gmail.com>
2024-09-24 12:14:35 +0800

    battery number

--- Game/Assets/Scripts/GameUI.cs
+++ Game/Assets/Scripts/GameUI.cs
@@ -1,13 +1,16 @@
 using System;
 using UnityEngine;
+using TMPro;
 
 public class GameUI : MonoBehaviour
 {
     [SerializeField] SpriteRenderer batIcon;
     [SerializeField] Sprite[] batStates;
+    [SerializeField] TextMeshProUGUI batNum;
     public float bat = 100;
 
     void Update() {
         batIcon.sprite = batStates[Mathf.CeilToInt(bat/25)];
+        batNum.text = Mathf.RoundToInt(bat).ToString() + "%";
     }
 }
