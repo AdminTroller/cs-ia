18fe6bf82f60d53c8931357436953dcf8e988290
AdminTroller <lauedmondyc@gmail.com>
2024-09-25 21:12:40 +0800

    minimap

---
+++ Game/Assets/Scripts/Minimap.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] Transform you;
+    [SerializeField] GameObject minimap;
+
+    float ratio = 0.1875f;
+
+    void Update() {
+        you.localPosition = player.position * ratio + new Vector3(0.9375f, -1.875f, 0);
+        minimap.SetActive(Input.GetKey(KeyCode.Tab));
+    }
+}
