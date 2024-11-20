3dfe5a5fa5630f232c4a58bb0d9b223c13a414d4
AdminTroller <lauedmondyc@gmail.com>
2024-11-20 20:17:50 +0800

    jumpscare continued

--- Game/Assets/Scripts/PlayerDeath.cs
+++ Game/Assets/Scripts/PlayerDeath.cs
@@ -11,9 +11,12 @@ public class PlayerDeath : MonoBehaviour
     [SerializeField] GameObject deathUI;
     [SerializeField] GameObject jumpscare;
     [SerializeField] AudioSource jumpscareSound;
+    [SerializeField] AudioSource staticSound;
+    [SerializeField] GameObject staticBg;
 
     bool dead = false;
     float deathTimer = 0;
+    const float deathTimerEnd = 1f;
 
     float zoomSpeed = 5f;
 
@@ -28,12 +31,28 @@ public class PlayerDeath : MonoBehaviour
             Pathfinding.state = 0;
             deathUI.SetActive(true);
             jumpscareSound.Play();
+
+            jumpscare.transform.localPosition = new Vector2(0,-8);
+            jumpscare.transform.eulerAngles = Vector3.zero;
+            jumpscare.transform.localScale = new Vector3(0.05f,0.05f,1);
         }
 
+        if (deathTimer > deathTimerEnd) {
+            jumpscareSound.Stop();
+            staticBg.SetActive(true);
+            if (!staticSound.isPlaying && deathTimer - deathTimerEnd < 1) staticSound.Play();
+            return;
+        }
+
+        if (jumpscare.transform.localPosition.y < -2) {
+            jumpscare.transform.localPosition += new Vector3(0,30f * Time.deltaTime);
+        }
         if (jumpscare.transform.localScale.x < 1) {
             jumpscare.transform.localScale += new Vector3(zoomSpeed*Time.deltaTime,zoomSpeed*Time.deltaTime);
-            jumpscare.transform.eulerAngles = new Vector3(0,0,Random.Range(-10f,10f));
+            zoomSpeed -= 12f * Time.deltaTime;
         }
+        jumpscare.transform.eulerAngles = new Vector3(0,0,Random.Range(-5f,5f));
+
         deathTimer += Time.deltaTime;
     }
 }
