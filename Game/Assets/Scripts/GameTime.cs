77b4721518299ca71f7bb1e8ca852e0c009da4fd
AdminTroller <lauedmondyc@gmail.com>
2024-11-24 20:37:02 +0800

    game time scale

--- Game/Assets/Scripts/GameTime.cs
+++ Game/Assets/Scripts/GameTime.cs
@@ -7,14 +7,15 @@ public class GameTime : MonoBehaviour
     public float t = 0;
     int tHour;
     string tMinutes;
-    float tScale = 0.01f;
+    float tScale = 0.02f;
 
     void Update() {
         if (t < 6) t += tScale * Time.deltaTime;
         tHour = Mathf.FloorToInt(t);
         tMinutes = Mathf.FloorToInt((t-tHour)*60).ToString();
         if (tHour == 0) tHour = 12;
-        if (tMinutes.Length == 1) tMinutes = "0" + tMinutes;
+        if (tMinutes.Length == 1) tMinutes = "00";
+        else tMinutes = tMinutes[0].ToString() + "0";
         tDisplay.text = tHour.ToString() + ":" + tMinutes + " AM";
     }
 }
