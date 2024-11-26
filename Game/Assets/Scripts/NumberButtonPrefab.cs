f02f4d92f015bade415bc6fbca49437bc72b5dcb
AdminTroller <lauedmondyc@gmail.com>
2024-11-26 10:23:49 +0800

    number buttons task

---
+++ Game/Assets/Scripts/NumberButtonPrefab.cs
@@ -0,0 +1,32 @@
+using TMPro;
+using UnityEngine;
+
+public class NumberButtonPrefab : MonoBehaviour
+{
+    public int number;
+    bool pressed = false;
+    [SerializeField] TextMeshProUGUI numberText;
+    [SerializeField] SpriteRenderer button;
+    [SerializeField] Sprite[] buttonSprites;
+    [SerializeField] BoxCollider2D buttonCollider;
+
+    void Start() {
+        numberText.text = number.ToString();
+    }
+
+    void Update() {
+        if (pressed) {
+            button.sprite = buttonSprites[2];
+            return;
+        }
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (buttonCollider.OverlapPoint(mousePos)) { // hovering cross
+            button.sprite = buttonSprites[1];
+            if (Input.GetMouseButtonDown(0) && NumberButtons.currentNumber == number) { // click
+                pressed = true;
+                NumberButtons.currentNumber++;
+            }
+        }
+        else button.sprite = buttonSprites[0];
+    }
+}
--- Game/Assets/Scripts/NumberButtons.cs
+++ Game/Assets/Scripts/NumberButtons.cs
@@ -1,16 +1,30 @@
+using System.Linq;
 using UnityEngine;
 
 public class NumberButtons : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
+    int[] numbers = Enumerable.Range(1,28).ToArray();
+    int index = 0;
+    public static int currentNumber = 1;
 
     void Start() {
+
+        for (int i=0; i<numbers.Length; i++) {
+            int temp = numbers[i];
+            int r = Random.Range(i, numbers.Length);
+            numbers[i] = numbers[r];
+            numbers[r] = temp;
+        }
+
         for (float x = -7.2f; x <= 7.201f; x+=2.4f) {
             for (float y = -3.6f; y <= 3.61f; y+=2.4f) {
                 GameObject current = Instantiate(prefab, gameObject.transform, false);
+                NumberButtonPrefab currentPrefab = current.GetComponent<NumberButtonPrefab>();
                 current.transform.localPosition = new Vector2(x,y);
+                currentPrefab.number = numbers[index];
+                index++;
             }
         }
-        
     }
 }
