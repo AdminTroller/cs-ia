85455f3f7936c0b5eea4760b78c2862c74c56acd
AdminTroller <lauedmondyc@gmail.com>
2024-11-30 17:40:59 +0800

    clamped camera movement

--- Game/Assets/Scripts/CameraMovement.cs
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -7,6 +7,6 @@ public class CameraMovement : MonoBehaviour
     [SerializeField] Transform player;
 
     void Update() {
-        transform.position = new Vector3(player.position.x, player.position.y, -10);
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, -30f, 17f), Mathf.Clamp(player.position.y, -17f, 32f), -10);
     }
 }
