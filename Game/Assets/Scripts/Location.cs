10178f37f301c1495efe75f92d2e3bbb0a5054ee
AdminTroller <lauedmondyc@gmail.com>
2024-12-03 20:11:39 +0800

    all locations

--- Game/Assets/Scripts/Location.cs
+++ Game/Assets/Scripts/Location.cs
@@ -6,7 +6,7 @@ public class Location : MonoBehaviour
     [SerializeField] TextMeshProUGUI locationText;
     [SerializeField] BoxCollider2D[] areas;
     [SerializeField] BoxCollider2D player;
-    string[] areaNames = {"Office","Storage"};
+    string[] areaNames = {"Office","Storage","Server Room","Right Generator","Ventilation","Bathrooms","Laboratory","Cellar","Left Generator","Exit"};
     string areaName = "Office";
 
     void Update() {
@@ -15,6 +15,7 @@ public class Location : MonoBehaviour
                 areaName = areaNames[i];
                 break;
             }
+            areaName = "";
         }
         locationText.text = areaName;
     }
