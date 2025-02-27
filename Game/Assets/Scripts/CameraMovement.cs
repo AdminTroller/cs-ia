bbe7a4f626556367ea4afd22e9bc825e49d1968b
AdminTroller <lauedmondyc@gmail.com>
2025-02-28 00:21:19 +0800

    MORE DAMN COMMENTS

--- Game/Assets/Scripts/CameraMovement.cs
+++ Game/Assets/Scripts/CameraMovement.cs
@@ -8,6 +8,6 @@ public class CameraMovement : MonoBehaviour
 
     void Update() { // make camera track player position
         transform.position = new Vector3(Mathf.Clamp(player.position.x, -30f, 17f), Mathf.Clamp(player.position.y, -17f, 32f), -10);
-        fpsTest.text = "FPS: " + (Mathf.Round(100 / Time.deltaTime)/100).ToString(); // debug FPS display
+        fpsTest.text = "FPS: " + (Mathf.Round(100 / Time.deltaTime)/100).ToString();
     }
 }
